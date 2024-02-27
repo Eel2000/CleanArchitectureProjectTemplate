df00b6d217397384898a9cbc42e3709c5d1b66d8
Eliezer BWANA <44249870+Eel2000@users.noreply.github.com>
2024-02-27 22:53:47 +0200

    add base controller api, and appsetting con string scope

---
+++ Src/CleanArchitectureProjectTemplate.WebApi/Controllers/BaseApiController.cs
@@ -0,0 +1,12 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchitectureProjectTemplate.WebApi.Controllers;
+
+[ApiController]
+[Route("api/v{version:apiVersion}/[controller]")]
+public class BaseApiController : ControllerBase
+{
+    private IMediator? mediator;
+    protected IMediator? Mediator => mediator ??= HttpContext?.RequestServices.GetService<IMediator>();
+}
